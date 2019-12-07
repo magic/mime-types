d999ad1f26c37089679ee536380d739b90615068
Jascha <jascha@jaeh.at>
2019-12-07 12:33:46 +0100

    update bin to also build docs

---
+++ bin/MAGIC-doc-template.mjs
@@ -0,0 +1,34 @@
+export const View = state => [
+  h1(state.title),
+  state.description.map(d => p(d)),
+
+  GitBadges('magic/mime-types'),
+
+  h3({ id: 'install' }, 'installation'),
+  p('be in a nodejs project'),
+  Pre(`
+npm install @magic/mime-types
+`),
+
+  h3({ id: 'usage' }, 'import / usage'),
+  Pre(`
+import mimes from '@magic/mime-types'
+
+||::mime-db-content::||
+`),
+
+  h4({ id: 'changelog' }, 'changelog'),
+
+  h5({ id: 'changelog-0.0.1' }, '0.0.1'),
+  p('first publish'),
+
+  h2({ id: 'source' }, 'source'),
+  p([
+    'the source for this page is in the ',
+    Link({ to: 'https://github.com/magic/fs/tree/master/example' }, 'example directory'),
+    ' and gets built and published to github using ',
+    Link({ to: 'https://github.com/magic/core' }, '@magic/core'),
+  ]),
+
+  LightSwitch(state),
+]
--- bin/bin.mjs
+++ bin/bin.mjs
@@ -10,20 +10,26 @@ const filePath = path.join(process.cwd(), 'src', 'index.mjs')
 
 const args = {
   options: [['--help', '-help', 'help', '--h', '-h']],
-  commands: ['build'],
+  commands: ['build', 'docs'],
   help: {
     name: 'magic-mime-types',
     header:
       'generates a list of key => values from mime-db. keys are extensions, values are mime/types',
     commands: {
+      docs: 'build README.md and the html documentation from mime-db data',
       build: 'build from mime-db to src/index.mjs',
     },
     example: `
 # build src/index.mjs from mime-db:
-src/bin/bin.js build
+./bin/bin.mjs build
+
+# recreate README.md and example/pages/index.mjs
+bin/bin.mjs docs
 `,
   },
 }
+
+import { default as mimes } from 'mime-db'
 
 const overwriteKeys = Object.keys(overwrites)
 
@@ -31,8 +37,6 @@ const run = async () => {
   const res = cli(args)
 
   if (res.cmds.build) {
-    const { default: mimes } = await import('mime-db')
-
     let keyValues = `export default {\n`
 
     Object.entries(mimes)
@@ -50,6 +54,43 @@ const run = async () => {
     keyValues += '}\n'
 
     await fs.writeFile(filePath, keyValues)
+  } else if (res.cmds.docs) {
+    let keyValues = ''
+
+    Object.entries(mimes)
+      .filter(([key, val]) => val.extensions && val.extensions.length)
+      .forEach(([key, val]) => {
+        val.extensions.forEach(ext => {
+          if (overwriteKeys.includes(ext)) {
+            keyValues += `
+console.log(mimes.${ext})
+// logs ${overwrites[ext]}
+`
+          } else {
+            keyValues += `
+console.log(mimes.${ext})
+// logs ${key}
+`
+          }
+        })
+      })
+
+    let docContent = await fs.readFile(
+      path.join(process.cwd(), 'bin', './README-template.md'),
+      'utf8',
+    )
+
+    docContent = docContent.replace('||::mime-db-content::||', keyValues)
+    await fs.writeFile(path.join(process.cwd(), 'README.md'), docContent)
+
+    let jsDocContent = await fs.readFile(
+      path.join(process.cwd(), 'bin', './MAGIC-doc-template.mjs'),
+      'utf8',
+    )
+
+    jsDocContent = jsDocContent.replace('||::mime-db-content::||', keyValues)
+
+    await fs.writeFile(path.join(process.cwd(), 'docsrc', 'pages', 'index.mjs'), jsDocContent)
   }
 }
 
