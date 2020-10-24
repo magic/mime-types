38d4b606350f876b387271684de3d68c477a984e
jascha <jascha@jaeh.at>
2020-10-24 21:30:05 +0200

    bin.mjs: add compressibles

--- bin/bin.mjs
+++ bin/bin.mjs
@@ -4,29 +4,36 @@ import path from 'path'
 
 import cli from '@magic/cli'
 import fs from '@magic/fs'
+import deep from '@magic/deep'
 
 import overwrites from './overwrites.mjs'
 
-const filePath = path.join(process.cwd(), 'src', 'index.mjs')
+const mimeTypeFilePath = path.join(process.cwd(), 'src', 'mimes.mjs')
+const compressFilePath = path.join(process.cwd(), 'src', 'compressibles.mjs')
 
 const args = {
   options: [['--help', '-help', 'help', '--h', '-h']],
   commands: ['build', 'docs'],
   help: {
     name: 'magic-mime-types',
-    header:
-      'generates a list of key => values from mime-db. keys are extensions, values are mime/types',
+    header: 'mime-db, sorted by file extension.',
     commands: {
-      docs: 'build README.md and the html documentation from mime-db data',
-      build: 'build from mime-db to src/index.mjs',
+      docs: 'build README.md and the html documentation from mime-db data. will also change src.',
+      build: 'build from mime-db to src',
     },
     example: `
-# build src/index.mjs from mime-db:
+# build src/ index.mjs from mime- db:
 ./bin/bin.mjs build
 
-# recreate README.md and example/pages/index.mjs
-bin/bin.mjs docs
-`,
+# recreate README.md and example / pages / index.mjs
+./bin/bin.mjs docs
+
+# create array of compressible extensions.
+./bin/bin.mjs compressible
+
+# run all tasks
+./bin/bin.mjs all
+  `,
   },
 }
 
@@ -36,25 +43,33 @@ const overwriteKeys = Object.keys(overwrites)
 
 const run = async () => {
   const { commands } = cli(args)
+  const mimeTypes = {}
+  const compressibles = {}
 
   if (commands.build) {
-    let keyValues = `export default {\n`
-
     Object.entries(mimes)
-      .filter(([key, val]) => val.extensions && val.extensions.length)
+      .filter(([key, val]) => val.extensions?.length)
       .forEach(([key, val]) => {
         val.extensions.forEach(ext => {
           if (overwriteKeys.includes(ext)) {
-            keyValues += `  '${ext}': '${overwrites[ext]}',\n`
+            mimeTypes[ext] = overwrites[ext]
           } else {
-            keyValues += `  '${ext}': '${key}',\n`
+            mimeTypes[ext] = key
+          }
+
+          if (val.compressible === true || val.compressible === false) {
+            compressibles[ext] = val.compressible
           }
         })
       })
 
-    keyValues += '}\n'
-
-    await fs.writeFile(filePath, keyValues)
+    await Promise.all([
+      fs.writeFile(mimeTypeFilePath, `export const mimes = ${JSON.stringify(mimeTypes, null, 2)}`),
+      fs.writeFile(
+        compressFilePath,
+        `export const compressibles = ${JSON.stringify(compressibles, null, 2)}`,
+      ),
+    ])
   }
 
   if (commands.docs) {
